931bec31a6492b7987e0f752bbcccdf61d5f1d90
Krzysztof-Wolinski <wolin51@gmail.com>
2021-06-26 15:00:17 +0200

    exercise init

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 import './style.css';
 
+// # Przeprogramowani.pl - Opanuj JavaScript
+
+// Moduł II. - Przeglądarka bez tajemnic
+
+// ## Ćwiczenie nr. 4 - Winda
+
+// ### Jak zacząć
+
+// Kliknij przycisk "Fork" u góry edytora aby skopiować ćwiczenie.
+
+// ### Cel ćwiczenia
+
+// Winda napędzana scrollem użytkownika porusza się w górę i w dół (zgodnie z kierunkiem scrollowania).
+
+// Spraw, aby pole nad windą, informujące o kierunku poruszania się, było aktualizowane na bieżąco.
+
+// ### Punkty bonusowe
+
+// 1. Spraw, aby na polu z informacjami pokazywało się również aktualne piętro
+
 const directions = {
   top: '🔼',
   bottom: '🔽'
-}
+};
